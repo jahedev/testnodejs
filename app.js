976c98d129b35c6eb4a5c0e49dd496d6d9b66bde
MJ Hossain <jahedev@gmail.com>
2023-05-23 22:19:44 -0400

    new changes

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 for (let i = 0; i < 10; i++) {
   console.log("hello world, ", i + 1)
+  console.log("hello world, ", i + 1)
+  console.log("hello world, ", i + 1)
+  console.log("hello world, ", i + 1)
+  console.log("hello world, ", i + 1)
 }
