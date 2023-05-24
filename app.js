46401b6785e9c4a836e4f41c0ba860ae3f8f5be8
MJ Hossain <jahedev@gmail.com>
2023-05-23 22:37:00 -0400

    simple express server

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-for (let i = 0; i < 10; i++) {
-  console.log("hello world, ", i + 1)
-  console.log("hello world, ", i + 1)
-  console.log("hello world, ", i + 1)
-  console.log("hello world, ", i + 1)
-  console.log("hello world, ", i + 1)
-}
+var colors = require("colors")
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World! Again!</h1>", 200)
+})
+
+app.get("/myfolder", (req, res) => {
+  res.send(
+    "<h1>You are unauthorized to access this</h1><img src='https://http.cat/401' />",
+    401
+  )
+})
+
+app.listen(port, () => {
+  console.log(colors.rainbow(`Example app listening on port ${port}`))
+})
